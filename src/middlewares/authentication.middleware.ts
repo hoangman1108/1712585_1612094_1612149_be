9d90d07c5183a18f99d65031b0155e531b26988a
DatNC <datnc@younetgroup.com>
2021-11-05 20:58:01 +0700

    auth for middlewares

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prefer-promise-reject-errors */
 import * as express from 'express';
+import httpStatus from 'http-status';
+import { TokenCollection } from '../models/token.model';
+import { ApiError } from '../utils';
+import authUtils from '../utils/auth';
 
 export async function expressAuthentication(
   request: express.Request,
@@ -9,6 +13,12 @@ export async function expressAuthentication(
 ): Promise<any> {
   if (securityName === 'oauth2') {
     const token = request.body.token || request.query.token || request.headers.authorization;
-    return {};
+    return authUtils.verifyJWT(token).then(async (result: any) => {
+      const itoken = await TokenCollection.findOne({ userId: result.userId });
+      if (!itoken) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'ACCOUNT_ACCESS_DENIED');
+      }
+      return result;
+    });
   }
 }
