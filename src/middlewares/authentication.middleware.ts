c5cc563dc1d0c35c777baeb3227bfd1ef9f991c5
hoangman1108 <hoangman772@gmail.com>
2021-12-30 20:26:06 +0700

    add passport

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -2,9 +2,37 @@
 /* eslint-disable prefer-promise-reject-errors */
 import * as express from 'express';
 import httpStatus from 'http-status';
+import { Error } from 'mongoose';
+import passport, { PassportStatic } from 'passport';
+import {
+  ExtractJwt, Strategy, StrategyOptions, VerifiedCallback,
+} from 'passport-jwt';
 import { TokenCollection } from '../models/token.model';
+import { UserAttributes, UserCollection } from '../models/user.model';
 import { ApiError } from '../utils';
 import authUtils from '../utils/auth';
+
+interface IJwtPayload {
+  user?: UserAttributes;
+  iat?: Date;
+}
+// eslint-disable-next-line @typescript-eslint/no-shadow
+export const authenticateUser = (passport: PassportStatic) => {
+  const options: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('authorization'),
+    secretOrKey: process.env.JWT_SECRET || 'secret',
+  };
+  passport.use(new Strategy(options, async (jwtPayload: IJwtPayload, done: VerifiedCallback) => {
+    // const result = await <IUserResponse>_userRepository.getUserById(jwtPayload.user._id);
+    const result = await UserCollection.findById(jwtPayload.user?._id);
+    console.log('jwtPayload.user?._id: ', jwtPayload.user?._id);
+    if (result instanceof Error) return done(result, false);
+    if (!result) {
+      return done(null, false);
+    }
+    return done(null, result, { issuedAt: jwtPayload.iat });
+  }));
+};
 
 export async function expressAuthentication(
   request: express.Request,
